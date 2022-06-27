7c1d7b695749cbbcb22140fe4b69cc4806cb8de7
kadjik <kadjik_gasparyan@mail.ru>
2022-06-27 13:37:22 +0300

    releae 1

--- src/View.js
+++ src/View.js
@@ -1,15 +1,22 @@
 // Сделаем отдельный класс для отображения игры в консоли.
+const { game } = require('./Game');
 
 class View {
-  render() {
-    const yourTeamName = 'Elbrus';
+  render(track,track1,track7,score,boom,speed) {
+    const yourTeamName = 'AKAI';
 
     // Тут всё рисуем.
     console.clear();
-    console.log(this.game.track.join(''));
+    console.log(track1.join(''));
+    console.log(track.join(' '));
+    console.log(track7.join(''));
+    console.log()
+    console.log('\n\n');
+    console.log(`Your Score  = ${score}. Бумерангов в наличии = ${boom} Скорость игры ${speed}`)
     console.log('\n\n');
     console.log(`Created by "${yourTeamName}" with love`);
   }
 }
+
 
 module.exports = View;
